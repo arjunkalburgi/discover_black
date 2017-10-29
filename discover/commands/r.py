51445b45dd25da6613406878b3d9af63f2432462
Arjun Kalburgi <askalburgi@gmail.com>
2017-10-29 00:58:19 -0500

    ask to narrow (rough)

--- discover/commands/r.py
+++ discover/commands/r.py
@@ -20,9 +20,34 @@ def run(question):
 
     # get actual title 
     title = data[u'query'][u'pages'][data[u'query'][u'pages'].keys()[0]][u'title'] # sorry
-    print(title)
-    
+    # print(title)
+
     # query wikidata for topics
+    level = 1
+    sections = narrow(title, level)
+    print("What about " + title + " would you like to learn?")
+    for sec in sections: 
+        print(sec["name"])
+
+    
+
+
+def narrow(title, level):
+    
+    # query wikidata for inner sections
+    api_url = "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=sections&page=" + title
+    response = requests.get(api_url)
+    if (response.status_code is not 200): 
+        print("fail")
+        quit()
+        
+    data = json.loads(response.content)
+
+    moretopics = []
+    for section in data[u'parse'][u'sections']: 
+        if (int(section[u'level']) is (level+1)): 
+            moretopics.append({"name": section[u'line'], "title": section[u'anchor']})
+    return moretopics
     
 
 run("I want to learn about Finance")
